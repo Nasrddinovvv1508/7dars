b4bb304806aa36eada3bac986c3d3463fc58f589
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-08-16 10:22:45 +0500

    home done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,11 +17,30 @@ function Home() {
             </p>
 
             <div className='mt-6 text-lg text-white'>
-              <p>You really want to know <span className='text-[#2b3440]'>about us</span></p>
-              <div className='mt-5'>
-                <Link to={`/about`} className='bg-red-600 hover:bg-red-800 py-2 px-4 rounded-lg text-white font-bold text-xl'>About</Link>
+              <p>You really want to know <span className='text-[#2b3440]'>about us</span> ??</p>
+              <div className='mt-5 pl-2'>
+                <Link to={`/about`} className='btn btn-error py-2 px-4 rounded-lg text-white font-bold text-xl'>About</Link>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section className='pt-[70px] bg-[#0977d6]'>
+        <div className='speacial-container grid grid-cols-2 items-center gap-[140px]'>
+          <div>
+            <h1 className='text-5xl font-bold text-white'>OR you trying to buy our <span className='text-gradient'>speacial products</span></h1>
+
+            <div className='mt-7'>
+              <button>
+                <Link to={`/products`} className='btn btn-secondary py-2 px-4 rounded-lg text-white font-bold text-xl'>Products</Link>
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <img src="../assets/gif/shelves.gif" alt="shelves" />
           </div>
         </div>
       </section>
