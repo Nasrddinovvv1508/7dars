b70cc2ddd14098ff114e16d47b134e3f0225e8cc
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-08-16 09:59:03 +0500

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import MainLayout from './layout/MainLayout'
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  let routes = createBrowserRouter([
+    {
+      path: `/`,
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: `/about`,
+          element: <About />
+        },
+        {
+          path: `/products`,
+          element: <Products />
+        }
+      ]
+    }
+  ])
+
+  return <RouterProvider router={routes} />
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="bg-base-300">
+      <div className='navbar speacial-container'>
+        <div className="flex-1">
+          <Link to={`/`} className="bg-red-600 py-2 px-4 rounded-lg text-white font-bold text-xl">Store</Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 text-lg gap-3">
+            <li><NavLink to={`/`}>Main</NavLink></li>
+            <li>
+              <details>
+                <summary>Pages</summary>
+                <ul className="bg-base-100 rounded-t-none p-2 w-[190px]">
+                  <li><NavLink to={`/about`}>About</NavLink></li>
+                  <li><NavLink to={`/products`}>Products</NavLink></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
